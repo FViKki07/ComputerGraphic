c1252c77e63bc184cab8e295886b5916cca25473
FViKki07 <73124000+FViKki07@users.noreply.github.com>
2023-09-28 17:51:58 +0300

    Create project

---
+++ Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba3
+{
+    public partial class Form2 : Form
+    {
+        Bitmap bmp;
+        bool DrawLine;
+        Point? prev;
+        Graphics g;
+
+        public Form2()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(pictureBox1.Image);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox1_MouseMove(sender, e);
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            g.DrawLine(new Pen(Color.FromArgb(255, 0, 0, 0), 1), prev.Value, e.Location);
+            prev = e.Location;
+            pictureBox1.Invalidate();
+        }
+    }
+}

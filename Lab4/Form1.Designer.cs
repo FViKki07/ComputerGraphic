f88a3d672875f842c01080db8b8d0c791667acdd
Anastasia <82052788+AnastasiaDb@users.noreply.github.com>
2023-10-05 20:03:56 +0300

    add rotation,dilatation , offset

--- Lab4/Form1.Designer.cs
+++ Lab4/Form1.Designer.cs
@@ -34,15 +34,33 @@
             LineRadioButton = new RadioButton();
             PolygonRadioButton = new RadioButton();
             button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            groupBox2 = new GroupBox();
+            button2 = new Button();
+            groupBox3 = new GroupBox();
+            textBox3 = new TextBox();
+            button3 = new Button();
+            label3 = new Label();
+            groupBox4 = new GroupBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            button4 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
             SuspendLayout();
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(285, 12);
+            pictureBox1.Location = new Point(326, 16);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(667, 516);
+            pictureBox1.Size = new Size(762, 688);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             pictureBox1.Paint += pictureBox1_Paint;
@@ -58,9 +76,11 @@
             groupBox1.Controls.Add(LineRadioButton);
             groupBox1.Controls.Add(PolygonRadioButton);
             groupBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Italic, GraphicsUnit.Point);
-            groupBox1.Location = new Point(12, 12);
+            groupBox1.Location = new Point(14, 16);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(252, 54);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(288, 72);
             groupBox1.TabIndex = 20;
             groupBox1.TabStop = false;
             groupBox1.Text = "Нарисовать:";
@@ -68,9 +88,10 @@
             // DotRadioButton
             // 
             DotRadioButton.AutoSize = true;
-            DotRadioButton.Location = new Point(6, 24);
+            DotRadioButton.Location = new Point(7, 32);
+            DotRadioButton.Margin = new Padding(3, 4, 3, 4);
             DotRadioButton.Name = "DotRadioButton";
-            DotRadioButton.Size = new Size(60, 21);
+            DotRadioButton.Size = new Size(75, 27);
             DotRadioButton.TabIndex = 12;
             DotRadioButton.TabStop = true;
             DotRadioButton.Text = "Точка";
@@ -79,9 +100,10 @@
             // LineRadioButton
             // 
             LineRadioButton.AutoSize = true;
-            LineRadioButton.Location = new Point(72, 24);
+            LineRadioButton.Location = new Point(82, 32);
+            LineRadioButton.Margin = new Padding(3, 4, 3, 4);
             LineRadioButton.Name = "LineRadioButton";
-            LineRadioButton.Size = new Size(79, 21);
+            LineRadioButton.Size = new Size(101, 27);
             LineRadioButton.TabIndex = 13;
             LineRadioButton.TabStop = true;
             LineRadioButton.Text = "Отрезок";
@@ -90,9 +112,10 @@
             // PolygonRadioButton
             // 
             PolygonRadioButton.AutoSize = true;
-            PolygonRadioButton.Location = new Point(163, 24);
+            PolygonRadioButton.Location = new Point(186, 32);
+            PolygonRadioButton.Margin = new Padding(3, 4, 3, 4);
             PolygonRadioButton.Name = "PolygonRadioButton";
-            PolygonRadioButton.Size = new Size(75, 21);
+            PolygonRadioButton.Size = new Size(96, 27);
             PolygonRadioButton.TabIndex = 14;
             PolygonRadioButton.TabStop = true;
             PolygonRadioButton.Text = "Полигон";
@@ -100,28 +123,175 @@
             // 
             // button1
             // 
-            button1.Location = new Point(75, 498);
+            button1.Location = new Point(86, 664);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(105, 30);
+            button1.Size = new Size(120, 40);
             button1.TabIndex = 21;
             button1.Text = "Очистить";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(41, 26);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(74, 27);
+            textBox1.TabIndex = 22;
+            textBox1.KeyPress += textBox1_KeyPress;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(177, 26);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(74, 27);
+            textBox2.TabIndex = 23;
+            textBox2.KeyPress += textBox2_KeyPress;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 20);
+            label1.TabIndex = 24;
+            label1.Text = "DX";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(148, 33);
+            label2.Name = "label2";
+            label2.Size = new Size(28, 20);
+            label2.TabIndex = 25;
+            label2.Text = "DY";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Location = new Point(21, 95);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(275, 97);
+            groupBox2.TabIndex = 26;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Сместить на:";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(82, 62);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 26;
+            button2.Text = "ок";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBox3);
+            groupBox3.Controls.Add(button3);
+            groupBox3.Controls.Add(label3);
+            groupBox3.Location = new Point(27, 213);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(245, 93);
+            groupBox3.TabIndex = 27;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Повернуть на:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(83, 26);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 2;
+            textBox3.KeyPress += textBox3_KeyPress;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(76, 59);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 1;
+            button3.Text = "ок";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 29);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 20);
+            label3.TabIndex = 0;
+            label3.Text = "Градусы:";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(label4);
+            groupBox4.Controls.Add(textBox4);
+            groupBox4.Controls.Add(button4);
+            groupBox4.Location = new Point(35, 331);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(237, 110);
+            groupBox4.TabIndex = 28;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Масштабировать:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(14, 33);
+            label4.Name = "label4";
+            label4.Size = new Size(35, 20);
+            label4.TabIndex = 3;
+            label4.Text = "На :";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(75, 26);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 2;
+            textBox4.KeyPress += textBox4_KeyPress;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(68, 75);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 1;
+            button4.Text = "Ок";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(964, 540);
+            ClientSize = new Size(1102, 720);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
             Controls.Add(button1);
             Controls.Add(groupBox1);
             Controls.Add(pictureBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             Shown += Form1_Shown;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -133,5 +303,19 @@
         private RadioButton LineRadioButton;
         private RadioButton PolygonRadioButton;
         private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private GroupBox groupBox2;
+        private Button button2;
+        private GroupBox groupBox3;
+        private TextBox textBox3;
+        private Button button3;
+        private Label label3;
+        private GroupBox groupBox4;
+        private TextBox textBox4;
+        private Button button4;
+        private Label label4;
     }
 }
--- Lab4/Form1.cs
+++ Lab4/Form1.cs
@@ -4,11 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 
 namespace Lab4
@@ -28,6 +30,8 @@ namespace Lab4
         bool drawLine;
         Point up;
         Point down;
+        int dx, dy;
+        double degree, k;
 
         public Form1()
         {
@@ -50,7 +54,7 @@ namespace Lab4
 
         private void Form1_Shown(object sender, EventArgs e)
         {
-            foreach (RadioButton RB in groupBox1.Controls)//.OfType<RadioButton>())
+            foreach (System.Windows.Forms.RadioButton RB in groupBox1.Controls)//.OfType<RadioButton>())
                 RB.Checked = false;
         }
 
@@ -155,6 +159,207 @@ namespace Lab4
             pictureBox1.Invalidate();
         }
 
+        private void offsetPolygon()
+        {
+            double[,] m = new double[3, 3]
+                {
+                  { 1, 0, dx },
+                  { 0, 1, -dy },
+                  {0, 0,  1 }
+                };
+
+            for (int i = 0; i < polygonPoints.Count; i++)
+            {
+
+                int x = polygonPoints[i].X;
+                int y = polygonPoints[i].Y;
+
+                int newX = (int)(m[0, 0] * x + m[0, 1] * y + m[0, 2]);
+                int newY = (int)(m[1, 0] * x + m[1, 1] * y + m[1, 2]);
+
+                polygonPoints[i] = new Point(newX, newY);
+            }
+
+            pictureBox1.Invalidate();
+
+        }
+
+
+        void rotatePolygon()
+        {
+            degree *= Math.PI / 180;
+            double cosD = Math.Cos(degree);
+            double sinD = Math.Sin(degree);
+
+            double[,] m = new double[3, 3]
+               {
+                  { cosD, sinD, 0 },
+                  { sinD, cosD, 0 },
+                  {0, 0,  1 }
+               };
+            int centerX = 0;
+            int centerY = 0;
+
+            for (int i = 0; i < polygonPoints.Count; i++)
+            {
+                centerX += polygonPoints[i].X;
+                centerY += polygonPoints[i].Y;
+            }
+
+            centerX /= polygonPoints.Count;
+            centerY /= polygonPoints.Count;
+
+            for (int i = 0; i < polygonPoints.Count; i++)
+            {
+                polygonPoints[i] = new Point(polygonPoints[i].X - centerX, polygonPoints[i].Y - centerY);
+            }
+
+            for (int i = 0; i < polygonPoints.Count; i++)
+            {
+                int x = polygonPoints[i].X;
+                int y = polygonPoints[i].Y;
+                int newX = (int)(m[0, 0] * x - m[0, 1] * y + m[0, 2]);
+                int newY = (int)(m[1, 0] * x + m[1, 1] * y + m[1, 2]);
+                polygonPoints[i] = new Point(newX, newY);
+            }
+
+            for (int i = 0; i < polygonPoints.Count; i++)
+            {
+                polygonPoints[i] = new Point(polygonPoints[i].X + centerX, polygonPoints[i].Y + centerY);
+            }
+
+            pictureBox1.Invalidate();
+
+        }
+
+        private void dilatationPolygon()
+        {
+            double[,] m = new double[3, 3]
+                          {
+                  { k, 0, 0 },
+                  { 0, k, 0 },
+                  {0, 0,  1 }
+             };
+
+            int centerX = 0;
+            int centerY = 0;
+
+            for (int i = 0; i < polygonPoints.Count; i++)
+            {
+                centerX += polygonPoints[i].X;
+                centerY += polygonPoints[i].Y;
+            }
+
+            centerX /= polygonPoints.Count;
+            centerY /= polygonPoints.Count;
+
+            for (int i = 0; i < polygonPoints.Count; i++)
+            {
+                polygonPoints[i] = new Point(polygonPoints[i].X - centerX, polygonPoints[i].Y - centerY);
+            }
+
+            for (int i = 0; i < polygonPoints.Count; i++)
+            {
+                int x = polygonPoints[i].X;
+                int y = polygonPoints[i].Y;
+                int newX = (int)(m[0, 0] * x + m[0, 1] * y + m[0, 2]);
+                int newY = (int)(m[1, 0] * x + m[1, 1] * y + m[1, 2]);
+                polygonPoints[i] = new Point(newX, newY);
+            }
+
+            for (int i = 0; i < polygonPoints.Count; i++)
+            {
+                polygonPoints[i] = new Point(polygonPoints[i].X + centerX, polygonPoints[i].Y + centerY);
+            }
+
+            pictureBox1.Invalidate();
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                dx = int.Parse(textBox1.Text);
+                dy = int.Parse(textBox2.Text);
+                offsetPolygon();
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text != "")
+            {
+                degree = double.Parse(textBox3.Text);
+                rotatePolygon();
+            }
+
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != '-')
+                e.Handled = true;
+
+            if (e.KeyChar == '-' && (sender as TextBox).Text.Length > 0)
+                e.Handled = true;
+
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != '-')
+                e.Handled = true;
+
+            if (e.KeyChar == '-' && (sender as TextBox).Text.Length > 0)
+                e.Handled = true;
+
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',' && e.KeyChar != '-')
+            {
+                e.Handled = true; // Запрещаем ввод других символов
+            }
+
+            if (e.KeyChar == '-' && (sender as TextBox).Text.Length > 0)
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == ',' && (sender as TextBox).Text.Contains(","))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',' )
+            {
+                e.Handled = true; // Запрещаем ввод других символов
+            }
+
+
+            if (e.KeyChar == ',' && (sender as TextBox).Text.Contains(","))
+            {
+                e.Handled = true;
+            }
+        }
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text != "")
+            {
+                k = double.Parse(textBox4.Text);
+                dilatationPolygon();
+            }
+        }
     }
 
 }

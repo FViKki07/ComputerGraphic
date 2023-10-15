af6987c3da48b1719d64c88b0cc51316310f26f6
Anastasia <82052788+AnastasiaDb@users.noreply.github.com>
2023-10-15 17:38:58 +0300

    update task2

--- Lab5/Form3.Designer.cs
+++ Lab5/Form3.Designer.cs
@@ -32,14 +32,18 @@
             button1 = new Button();
             label1 = new Label();
             button2 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            label2 = new Label();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(152, 12);
+            pictureBox1.Location = new Point(211, 12);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(636, 426);
+            pictureBox1.Size = new Size(679, 481);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             pictureBox1.MouseDown += pictureBox1_MouseDown;
@@ -59,7 +63,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(12, 66);
+            label1.Location = new Point(12, 22);
             label1.Name = "label1";
             label1.Size = new Size(96, 25);
             label1.TabIndex = 3;
@@ -68,7 +72,7 @@
             // button2
             // 
             button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(12, 120);
+            button2.Location = new Point(12, 118);
             button2.Name = "button2";
             button2.Size = new Size(114, 36);
             button2.TabIndex = 4;
@@ -76,11 +80,39 @@
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(16, 60);
+            numericUpDown1.Maximum = new decimal(new int[] { 30, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(130, 27);
+            numericUpDown1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 175);
+            label2.Name = "label2";
+            label2.Size = new Size(182, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Выполненных итераций:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(90, 206);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 20);
+            label3.TabIndex = 7;
+            // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(902, 505);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(numericUpDown1);
             Controls.Add(button2);
             Controls.Add(label1);
             Controls.Add(button1);
@@ -88,6 +120,7 @@
             Name = "Form3";
             Text = "Form3";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -98,5 +131,8 @@
         private Button button1;
         private Label label1;
         private Button button2;
+        private NumericUpDown numericUpDown1;
+        private Label label2;
+        private Label label3;
     }
 }
--- Lab5/Form3.cs
+++ Lab5/Form3.cs
@@ -21,9 +21,15 @@ namespace Lab5
         private Bitmap bmp;
         int iteration;
         Random random = new Random();
+        int x;
+        float l;
+        int currentIteration;
         public Form3()
         {
             InitializeComponent();
+            currentIteration = 0;
+            l = 5;
+            x = 1;
             pb = pictureBox1;
             bmp = new Bitmap(pb.Width, pb.Height);
             pictureBox1.Image = bmp;
@@ -43,52 +49,65 @@ namespace Lab5
             }
 
         }
-        private void getNewPoint(PointF p1, PointF p2)
+        private PointF getNewPoint(PointF p1, PointF p2)
         {
-            iteration++;
-            float l = (float)Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));//длина отрезка
-            if (l < 1)
-            {
-                label1.Text = "Итераций :" + iteration.ToString();
-                return;
-            }
+            l = (float)Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));//длина отрезка
             int ind = (int)Math.Floor(l * 0.3);
             float newX = (p1.X + p2.X) / 2;
             float newY = (float)((p1.Y + p2.Y) / 2 + random.Next(-ind, ind + 1));
-            PointF newPoint = new PointF(newX, newY);
-            pointLocation.Add(newPoint);
-            getNewPoint(p1, newPoint);
-            getNewPoint(newPoint, p2);
+            if (newY > pb.Height)
+                newY = pb.Height - 5;
+            return new PointF(newX, newY);
         }
 
         private void paintPoint()
         {
-            iteration = 0;
-            getNewPoint(pointLocation[0], pointLocation[1]);
-            pointLocation = pointLocation.OrderBy(p => p.X).ToList();
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(pictureBox1.Image);
+            // int x = 1;
+            for (int i = 0; i < iteration; i++)
+            {
+                g.Clear(Color.White);
+                for (int j = 0; j < x; j++)
+                {
+                    PointF newPoint = getNewPoint(pointLocation[j], pointLocation[j + 1]);
+                    pointLocation.Add(newPoint);
+                    if (l < 1)
+                    {
+                        label3.Text = currentIteration.ToString();
+                        return;
+                    }
+                }
+                currentIteration++;
+                pointLocation = pointLocation.OrderBy(x => x.X).ToList();
+                x *= 2;
+                g.DrawLines(Pens.Black, pointLocation.ToArray());
+                pictureBox1.Refresh();
+                Thread.Sleep(500);
+            }
+
             g.DrawLines(Pens.Black, pointLocation.ToArray());
             pictureBox1.Invalidate();
-        }
+            label3.Text = currentIteration.ToString();
 
-        //private void DrawPoint()
-        //{
-        //    g.DrawLines(Pens.Black, pointLocation.ToArray());
-        //    pictureBox1.Invalidate();
-        //}
+        }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            iteration = (int)numericUpDown1.Value;
             //MessageBox.Show(iteration.ToString());
             if (pointLocation.Count == 2)
                 paintPoint();
-            else MessageBox.Show("Отметьте 2 точки");
+
+
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            x = 1;
             pointLocation.Clear();
             g.Clear(Color.White);
             pictureBox1.Invalidate();
-            label1.Text = "Итераций:";
+            currentIteration = 0;
         }
 
     }

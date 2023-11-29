45e95c7906b4ed68297d3524d125d92fbe5454ee
FViKki07 <73124000+FViKki07@users.noreply.github.com>
2023-11-29 21:22:22 +0300

    Update main.cpp

--- Lab12/main.cpp
+++ Lab12/main.cpp
@@ -10,9 +10,6 @@
 #include <array>
 #include <initializer_list>
 
-#define deg2rad M_PI /180.0
-
-
 // Переменные с индентификаторами ID
 // ID шейдерной программы
 GLuint Program;
@@ -22,7 +19,7 @@ GLint Attrib_vertex;
 GLint Attrib_color;
 // ID юниформ переменной цвета
 GLint Unif_xmove;
-
+GLint Unif_zmove;
 GLint Unif_ymove;
 // ID VBO вершин
 GLuint VBO_position;
@@ -45,12 +42,13 @@ const char* VertexShaderSource = R"(
 
     uniform float x_move;
     uniform float y_move;
-    
+    uniform float z_move;
+
     out vec4 vert_color;
 
     void main() {
-        vec3 position = vec3(coord) + vec3(x_move, y_move, 0);
-        gl_Position = vec4(position[0], position[1], 0.0, 1.0);
+        vec3 position = vec3(coord) + vec3(x_move, y_move, z_move);
+        gl_Position = vec4(position, 1.0);
         vert_color = color;
     }
 )";
@@ -73,52 +71,11 @@ void Release();
 
 float moveX = 0;
 float moveY = 0;
-
-void moveShape(float moveXinc, float moveYinc) {
+float moveZ = 0;
+void moveShape(float moveXinc, float moveYinc, float moveZinc) {
     moveX += moveXinc;
     moveY += moveYinc;
-}
-
-int main() {
-    sf::Window window(sf::VideoMode(600, 600), "My OpenGL window", sf::Style::Default, sf::ContextSettings(24));
-    window.setVerticalSyncEnabled(true);
-
-    window.setActive(true);
-
-    // Инициализация glew
-    glewInit();
-
-    Init();
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-            else if (event.type == sf::Event::Resized) {
-                glViewport(0, 0, event.size.width, event.size.height);
-            }
-            else if (event.type == sf::Event::KeyPressed) {
-                switch (event.key.code) {
-                case (sf::Keyboard::W): moveShape(0, 0.1); break;
-                case (sf::Keyboard::S): moveShape(0, -0.1); break;
-                case (sf::Keyboard::A): moveShape(-0.1, 0); break;
-                case (sf::Keyboard::D): moveShape(0.1, 0); break;
-                default: break;
-                }
-            }
-        }
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        Draw();
-
-        window.display();
-    }
-
-    Release();
-    return 0;
+    moveZ += moveZinc;
 }
 
 
@@ -239,6 +196,13 @@ void InitShader() {
         return;
     }
 
+    unif_name = "z_move";
+    Unif_zmove = glGetUniformLocation(Program, unif_name);
+    if (Unif_zmove == -1)
+    {
+        std::cout << "could not bind uniform " << unif_name << std::endl;
+        return;
+    }
     checkOpenGLerror();
 }
 
@@ -254,7 +218,7 @@ void Draw() {
 
     glUniform1f(Unif_xmove, moveX);
     glUniform1f(Unif_ymove, moveY);
-
+    glUniform1f(Unif_zmove, moveZ);
 
     // Включаем массивы атрибутов
     glEnableVertexAttribArray(Attrib_vertex);
@@ -302,4 +266,58 @@ void ReleaseVBO()
 void Release() {
     ReleaseShader();
     ReleaseVBO();
+}
+
+void WindowWork(int num_task) {
+    sf::Window window(sf::VideoMode(600, 600), "My OpenGL window", sf::Style::Default, sf::ContextSettings(24));
+    window.setVerticalSyncEnabled(true);
+
+    window.setActive(true);
+
+    // Инициализация glew
+    glewInit();
+
+    Init();
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::Resized) {
+                glViewport(0, 0, event.size.width, event.size.height);
+            }
+            else if (event.type == sf::Event::KeyPressed) {
+                switch (event.key.code) {
+                case (sf::Keyboard::W): moveShape(0, 0.1, 0); break;
+                case (sf::Keyboard::S): moveShape(0, -0.1, 0); break;
+                case (sf::Keyboard::A): moveShape(-0.1, 0, 0); break;
+                case (sf::Keyboard::D): moveShape(0.1, 0, 0); break;
+                case (sf::Keyboard::Q): moveShape(0, 0, -0.2); break;
+                case (sf::Keyboard::E): moveShape(0, 0, 0.2); break;
+                default: break;
+                }
+            }
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        Draw();
+
+        window.display();
+    }
+
+    Release();
+}
+
+int main() {
+    int num_task = 0;
+    while (true) {
+        std::cout << "Enter task: ";
+        std::cin >> num_task;
+        if (num_task == 4 || num_task == 2 || num_task == 3 || num_task == 1)
+            WindowWork(num_task);
+    }
+    return 0;
 }
